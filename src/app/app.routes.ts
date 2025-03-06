94aab94ded0275aaf380688beff2097114127c75
alinenink <116383618+alinenink@users.noreply.github.com>
2025-03-06 17:16:49 -0300

    atualização das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { BoletoComponent } from './components/boleto/boleto.component';
-import { AdiantarParcelasPopupComponent } from './components/adiantar-parcelas-popup/adiantar-parcelas-popup.component';
-import { QuitarContratoPopupComponent } from './components/quitar-contrato-popup/quitar-contrato-popup.component';
+import { ControlePrestacoesComponent } from './components/controle-prestacoes/controle-prestacoes.component';
+import { AdiantarPrestacoesComponent } from './components/adiantar-prestacoes/adiantar-prestacoes.component';
+import { QuitarContratoComponent } from './components/quitar-contrato/quitar-contrato.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, title: 'Login' },
   { path: 'home', component: HomeComponent },
-  { path: 'gerar-boleto', component: BoletoComponent, data: { animation: 'GerarBoletoPage' } },
-  { path: 'adiantar-parcelas', component: AdiantarParcelasPopupComponent, data: { animation: 'AdiantarParcelasPage' } },
-  { path: 'quitar-contrato', component: QuitarContratoPopupComponent, data: { animation: 'QuitarContratoPage' } },
+  { path: 'gerar-boleto', component: ControlePrestacoesComponent, data: { animation: 'GerarBoletoPage' } },
+  { path: 'adiantar-parcelas', component: AdiantarPrestacoesComponent, data: { animation: 'AdiantarParcelasPage' } },
+  { path: 'quitar-contrato', component: QuitarContratoComponent, data: { animation: 'QuitarContratoPage' } },
 ];
 
 export const appRouter = [provideRouter(routes)];
