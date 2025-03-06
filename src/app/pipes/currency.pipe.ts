bc5baf788f686e4a7e227061876baed568264591
alinenink <116383618+alinenink@users.noreply.github.com>
2025-03-06 17:10:02 -0300

    pipes para formatação de data e moeda

---
+++ src/app/pipes/currency.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customCurrency'
+})
+export class CustomCurrencyPipe implements PipeTransform {
+  transform(value: number): string {
+    if (typeof value !== 'number') {
+      return '';
+    }
+    return value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  }
+}
